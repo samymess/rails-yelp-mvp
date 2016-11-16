2a0db242d50a5b0adf2abab5c283831dcdf18bb1
Samy Messaoudi <samymessaoudi37@gmail.com>
2016-11-16 14:37:18 +0100

    add restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Bruce Lee & Sushi",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number:  0145587412,
+    category:      "japanese"
+
+  },
+  {
+
+    name:         "Luca Brazzi",
+    address:      "112 place d'Italie 75014 Paris",
+    phone_number:  0145587413,
+    category:      "italien"
+  },
+  {
+
+    name:         "Amaterasu",
+    address:      "112 avenue Lamarck 75018 Paris",
+    phone_number:  0145587415,
+    category:      "chinese"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
