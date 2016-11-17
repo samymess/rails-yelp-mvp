1a517f4a06626d0b9e329ef36050d792493223fc
Samy Messaoudi <samymessaoudi37@gmail.com>
2016-11-17 11:45:49 +0100

    yelp

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :Restaurant
+  RATING = (0..5).to_a
+  validates :rating, inclusion: { in: RATING,
+message: "%{value} is not a valid rating" }
   validates :content, presence: true, allow_blank: false
   validates :rating, presence: true, allow_blank: false
 end
